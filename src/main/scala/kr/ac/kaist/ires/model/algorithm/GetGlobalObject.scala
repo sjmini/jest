2f3b89ace417a80f953259b697b59569194c2c78
jhnaldo <jhpjhp0223@gmail.com>
2020-04-14 19:07:33 +0900

    Applied updated semantics

--- src/main/scala/kr/ac/kaist/ires/model/algorithm/GetGlobalObject.scala
+++ src/main/scala/kr/ac/kaist/ires/model/algorithm/GetGlobalObject.scala
@@ -6,8 +6,7 @@ import kr.ac.kaist.ires.ir.Parser._
 object GetGlobalObject {
   val length: Int = 0
   val func: Func = parseFunc(""""GetGlobalObject" () => {
-    let ctx = GLOBAL_context
-    let currentRealm = ctx["Realm"]
+    let currentRealm = REALM
     app __x0__ = (WrapCompletion currentRealm["GlobalObject"])
     return __x0__
   }""")
