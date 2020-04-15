4b243e4d7ed0935db9c867a0743d6b451050faab
jhnaldo <jhpjhp0223@gmail.com>
2020-04-16 04:26:15 +0900

    Applied updated semantics

--- src/main/scala/kr/ac/kaist/ires/phase/FilterMeta.scala
+++ src/main/scala/kr/ac/kaist/ires/phase/FilterMeta.scala
@@ -129,10 +129,11 @@ case object FilterMeta extends PhaseObj[Unit, FilterMetaConfig, Unit] {
     )).remove("locale", !_.locales.isEmpty)
     .remove("negative", !_.negative.isEmpty)
 
-  lazy val test262configSummary = getTests(standardFeatures).getSummary
-  lazy val test262propconfigSummary =
-    getTests("optional-chaining" :: standardFeatures)
-      .remove("non optional-chaining", m => !(m.features contains "optional-chaining"))
+  lazy val test262configSummary =
+    getTests("String.prototype.matchAll" :: "Symbol.matchAll" :: standardFeatures)
+      .remove("non optional-chaining", m =>
+        !(m.features contains "String.prototype.matchAll") &&
+          !(m.features contains "Symbol.matchAll"))
       .getSummary
 
   def apply(
